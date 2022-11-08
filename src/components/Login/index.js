d46bd8e02c1b407ecdef6d1e15c4a40011f2609a
root <a.harish3105@gmail.com>
2022-11-08 05:44:20 +0000

    CodingPractice5 LoginApp

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+// Write your code here
+import './index.css'
+
+const Login = props => {
+  const {isClickLogin} = props
+  return (
+    <button type="button" className="login-button" onClick={isClickLogin}>
+      Login
+    </button>
+  )
+}
+
+export default Login
---
+++ src/components/Logout/index.js
@@ -0,0 +1,13 @@
+// Write your code here
+import './index.css'
+
+const Logout = props => {
+  const {isClickLogout} = props
+  return (
+    <button type="button" className="logout-button" onClick={isClickLogout}>
+      Logout
+    </button>
+  )
+}
+
+export default Logout
